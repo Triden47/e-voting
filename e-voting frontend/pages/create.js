be8b51e0a84cac1a48ff66a5a13dc1954b9cdbce
Triden47 <rajputashwin011@gmail.com>
2022-10-26 09:58:37 +0530

    create poll page redesign

--- e-voting frontend/pages/create.js
+++ e-voting frontend/pages/create.js
@@ -1,13 +1,122 @@
+import {
+  Formik,
+  Field,
+  Form,
+  ErrorMessage,
+  FieldArray,
+  useFormik,
+} from "formik";
+import { Button, Input } from "@chakra-ui/react";
+
+import SimpleSidebar from "../components/Sidebar";
+
+const initialValues = {
+  title: "",
+  candidates: [
+    {
+      name: "",
+      email: "",
+    },
+  ],
+};
+
 const create = () => {
   return (
-    <>
-      <FormControl>
-        <FormLabel>Email address</FormLabel>
-        <Input type="email" />
-        <FormHelperText>We'll never share your email.</FormHelperText>
-      </FormControl>
-    </>
-  );
-};
+    <div>
+      <SimpleSidebar />
+    </div>
+  )
+}
+// const create = () => {
+//   const formik = useFormik({
+//     initialValues: {
+//       title: "",
+//     },
+//     onSubmit: (values) => {
+//       alert(JSON.stringify(values, null, 2));
+//     },
+//   });
+//   return (
+//     <div>
+//       <Formik
+//         initialValues={initialValues}
+//         onSubmit={async (values) => {
+//           await new Promise((r) => setTimeout(r, 500));
+//           alert(JSON.stringify(values, null, 2));
+//         }}
+//       >
+//         {({ values }) => (
+//           <Form>
+//             <label htmlFor="title">Poll Title</label>
+//             <Field
+//               as={Input}
+//               id="title"
+//               name="title"
+//               type="text"
+//             />
+
+//             <FieldArray name="candidates">
+//               {({ insert, remove, push }) => (
+//                 <div>
+//                   {values.candidates.length > 0 &&
+//                     values.candidates.map((candidate, index) => (
+//                       <div className="row" key={index}>
+//                         <label>Candidate {index + 1}</label>
+//                         <div className="col">
+//                           <label htmlFor={`candidates.${index}.name`}>Name</label>
+//                           <Field
+//                             as={Input}
+//                             name={`candidates.${index}.name`}
+//                             placeholder="Jane Doe"
+//                             type="text"
+//                           />
+//                           <ErrorMessage
+//                             name={`candidates.${index}.name`}
+//                             component="div"
+//                             className="field-error"
+//                           />
+//                         </div>
+//                         <div className="col">
+//                           <label htmlFor={`candidates.${index}.email`}>
+//                             Email
+//                           </label>
+//                           <Field
+//                             as={Input}
+//                             name={`candidates.${index}.email`}
+//                             placeholder="jane@acme.com"
+//                             type="email"
+//                           />
+//                           <ErrorMessage
+//                             name={`candidates.${index}.email`}
+//                             component="div"
+//                             className="field-error"
+//                           />
+//                         </div>
+//                         <div className="col">
+//                           <Button
+//                             type="button"
+//                             onClick={() => remove(index)}
+//                           >
+//                             X
+//                           </Button>
+//                         </div>
+//                       </div>
+//                     ))}
+//                   <Button
+//                     type="button"
+//                     onClick={() => push({ name: "", email: "" })}
+//                   >
+//                     Add candidate
+//                   </Button>
+//                 </div>
+//               )}
+//             </FieldArray>
+//             <Button type="submit">Submit</Button>
+//           </Form>
+//         )}
+//       </Formik>
+//     </div>
+//   );
+// };
 
 export default create;
